289b4d5e8805c2f90bbf428525a8d9a180ecb6ee
OjinHyeok <ojh5266@gameberry.co.kr>
2026-04-01 13:54:40 +0900

    DK 배경 추가
    DK 배경 추가

---
+++ PTT/Assets/Main/Scripts/FrameWork/Controller/Managers/Dungeon/BGManager/BGLayerElement.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameBerry
+{
+    public class BGLayerElement : MonoBehaviour
+    {
+        //private BGLayerData m_myLayerData;
+
+        //private Queue<Transform> m_myLayerBGPool = new Queue<Transform>();
+
+        //private float m_minimumXSize;
+
+        //private SpriteRenderer m_myspriteRenderer;
+
+        ////------------------------------------------------------------------------------------
+        //public void Init(float minimumXSize)
+        //{
+        //    GameObject bgsprite = new GameObject();
+        //    m_myspriteRenderer = bgsprite.AddComponent<SpriteRenderer>();
+        //    m_myspriteRenderer.transform.SetParent(transform);
+
+        //    m_minimumXSize = minimumXSize;
+        //}
+        ////------------------------------------------------------------------------------------
+        //public void SetElementData(BGLayerData layerdata)
+        //{
+        //    m_myLayerData = layerdata;
+
+
+        //}
+        ////------------------------------------------------------------------------------------
+        //public void ResetGroupTransform()
+        //{ 
+
+        //}
+        ////------------------------------------------------------------------------------------
+    }
+}
